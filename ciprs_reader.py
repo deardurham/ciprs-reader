0b969f4a6a739e25d8c9ce49c323e004ea189fe0
Colin Copeland <copelco@caktusgroup.com>
2019-06-22 13:27:53 -0400

    bump to v0.0.2

--- ciprs_reader.py
+++ ciprs_reader.py
@@ -2,6 +2,7 @@ import argparse
 import logging
 import sys
 
+from ciprs import VERSION
 from ciprs.reader import PDFToTextReader
 
 
@@ -13,6 +14,7 @@ if __name__ == "__main__":
                         help="increases log verbosity for each occurence.")
     parser.add_argument('--source', help='Include full source in JSON output',
                         action='store_true')
+    parser.add_argument('--version', action='version', version='%(prog)s ' + VERSION)
 
     args = parser.parse_args()
     formatter = logging.Formatter('%(levelname)s %(asctime)s %(name)s %(message)s')
