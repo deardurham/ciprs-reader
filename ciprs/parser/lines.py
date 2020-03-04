db0cdef85d3787a7cd46443ee51506ef2a5fc591
Colin Copeland <copelco@caktusgroup.com>
2020-03-03 21:12:41 -0500

    remove Parser class

--- ciprs/parser/lines.py
+++ ciprs/parser/lines.py
@@ -5,74 +5,9 @@ import logging
 import re
 
 from ciprs import DISPOSITION_CODES
+from ciprs.parser.base import Parser
 
 logger = logging.getLogger(__name__)
-
-
-class Parser:
-    """Base parsing object to search and extract data into report"""
-
-    # Regular expression search pattern
-    pattern = None
-    re_method = "match"
-    # Default location in report to save match, expressed as tuple
-    # For example:
-    #   ("Case Information", "Case Status")
-    #   will save to:
-    #   report['Case Information']['Case Status']
-    section = []
-    is_line_parser = True
-
-    def __init__(self, report=None):
-        self.re = re.compile(self.pattern)
-        self.report = report
-        self.matches = None
-        self.document = None
-        name = f"{__name__}.{self.__class__.__name__}"
-        self.logger = logging.getLogger(name)
-
-    def match(self, document):
-        """Search for match in document"""
-        self.matches = None
-        self.document = document
-        if self.re_method == "match":
-            matches = self.re.match(str(document))
-        else:
-            matches = self.re.search(str(document))
-        if matches:
-            matches = matches.groupdict()
-            # strip whitespace on any values
-            for key, val in matches.items():
-                matches[key] = val.strip()
-            if self.is_line_parser:
-                self.logger.info("Matched: %s in %s", matches, str(document).strip())
-            else:
-                self.logger.info("Matched: %s in (document)", matches)
-            self.matches = self.clean(matches)
-        else:
-            if self.is_line_parser:
-                self.logger.debug("No match: %s", str(document).strip())
-            else:
-                self.logger.debug("No match: (document)")
-        return self.matches
-
-    def clean(self, matches):
-        """Overridable hook to clean matches"""
-        return matches
-
-    def find(self, document):
-        """Look for match and run extract() if found"""
-        self.match(document)
-        if self.matches:
-            # import ipdb; ipdb.set_trace()
-            self.extract(self.matches, self.report)
-
-    def extract(self, matches, report):
-        """
-        Update report with match, defaulting to a named match "value".
-        Override this function for more specific actions.
-        """
-        report[self.section[0]][self.section[1]] = matches["value"]
 
 
 class CaseDetails(Parser):
