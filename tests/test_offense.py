ce609b1616fe8b202d692956f47540e17b914f1e
Colin Copeland <copelco@caktusgroup.com>
2020-06-13 08:26:46 -0400

    add offense tests

---
+++ tests/test_offense.py
@@ -0,0 +1,32 @@
+import pytest
+
+from ciprs.parser.offense import Offenses, Offense
+
+
+@pytest.fixture
+def offense():
+    return Offense()
+
+
+@pytest.fixture
+def offenses():
+    return Offenses()
+
+
+def test_offenses_starts_empty(offenses):
+    assert len(offenses) == 0
+
+
+def test_offenses_new(offenses):
+    offenses.new()
+    assert len(offenses) == 1
+
+
+def test_offenses_current_always_exists(offenses):
+    assert offenses.current == {}
+
+
+def test_add_record(offense):
+    record = {"foo": "bar"}
+    offense.add_record(record)
+    assert offense["Records"][-1] == record
