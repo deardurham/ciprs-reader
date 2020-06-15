57ab59f050be25895bc495528d714d6d33ed8dee
Colin Copeland <copelco@caktusgroup.com>
2020-06-14 21:14:14 -0400

    add docstrings; remove unused method

--- ciprs_reader/parser/models.py
+++ ciprs_reader/parser/models.py
@@ -2,7 +2,10 @@ import collections
 
 
 class Offenses(collections.UserList):
-    """Simple list wrapper to manage Offense objects more easily."""
+    """
+    Simple list wrapper to manage Offense objects more easily and
+    is still easily JSON serializable.
+    """
 
     @property
     def current(self):
@@ -14,15 +17,15 @@ class Offenses(collections.UserList):
         self.data.append(Offense())
         return self.current
 
-    def add_disposition_method(self, method):
-        self.data[-1]["Disposition Method"] = method
-
     def __json__(self):
         return self.data
 
 
 class Offense(collections.UserDict):
-    """Offense wrapper to easily add new records."""
+    """
+    Offense wrapper to easily add new records and is still easily
+    JSON serializable.
+    """
 
     def add_record(self, record):
         if "Records" not in self.data:
