8e0968e2a073b86eec944b79c87822ce3d04ecb8
Colin Copeland <copelco@caktusgroup.com>
2020-06-14 21:41:39 -0400

    add docstrings

--- ciprs_reader/reader/main.py
+++ ciprs_reader/reader/main.py
@@ -12,8 +12,11 @@ logger = logging.getLogger(__name__)
 
 
 class PDFToTextReader:
+    """Read PDF and perform entity extraction using parsers."""
+
     def __init__(self, path):
         self.path = path
+        # skelton JSON structure for parser-extracted entities
         self.report = {
             "General": {},
             "Case Information": {},
@@ -22,13 +25,16 @@ class PDFToTextReader:
             "Superior Court Offense Information": Offenses(),
             "_meta": {},
         }
-        state = ParserState()
+        # object that stores state that's accessible between parsers
+        self.state = ParserState()
+        # line parsers are run against every line within a document
         self.line_parsers = []
         for parser in LINE_PARSERS:
-            self.line_parsers.append(parser(self.report, state))
+            self.line_parsers.append(parser(self.report, self.state))
         self.document_parsers = []
+        # document parsers are run once against an entire document
         for parser in DOCUMENT_PARSERS:
-            self.document_parsers.append(parser(self.report, state))
+            self.document_parsers.append(parser(self.report, self.state))
 
     def convert_to_text(self):
         run = subprocess.run(
