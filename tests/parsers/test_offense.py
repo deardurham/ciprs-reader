11101fc723131bec9b7b7f2cc55e71ad8067102f
Rob Gries <robert.w.gries@gmail.com>
2021-05-01 19:38:42 -0400

    Use multiline mode for offense record tests

--- tests/parsers/test_offense.py
+++ tests/parsers/test_offense.py
@@ -1,11 +1,12 @@
-import re
 import pytest
 
 from ciprs_reader.parser.section import offense
 
 
 def test_offense_record_charged(report, state):
-    string = "CHARGED       SPEEDING(80 mph in a 65 mph zone)    INFRACTION    G.S. 20-141(B)"  # noqa
+    string = """
+        CHARGED     SPEEDING(80 mph in a 65 mph zone)    INFRACTION    G.S. 20-141(B)"
+    """
     matches = offense.OffenseRecordRow(report, state, multiline=True).match(string)
     assert matches is not None, "Regex match failed"
     assert matches["action"] == "CHARGED"
@@ -14,15 +15,11 @@ def test_offense_record_charged(report, state):
     assert matches["law"] == "G.S. 20-141(B)"
 
 
-@pytest.mark.parametrize(
-    "line",
-    (
-        "54  CHARGED       SPEEDING(80 mph in a 65 mph zone)    INFRACTION    G.S. 20-141(B)",
-        "  54  CHARGED       SPEEDING(80 mph in a 65 mph zone)    INFRACTION    G.S. 20-141(B)   ",
-    ),
-)
-def test_offense_record_charged_with_number(line, report, state):
-    matches = offense.OffenseRecordRowWithNumber(report, state).match(line)
+def test_offense_record_charged_with_number(report, state):
+    string = """
+        54  CHARGED       SPEEDING(80 mph in a 65 mph zone)    INFRACTION    G.S. 20-141(B)    
+    """
+    matches = offense.OffenseRecordRowWithNumber(report, state, multiline=True).match(string)
     assert matches is not None, "Regex match failed"
     assert matches["action"] == "CHARGED"
     assert matches["desc"] == "SPEEDING(80 mph in a 65 mph zone)"
@@ -31,8 +28,10 @@ def test_offense_record_charged_with_number(line, report, state):
 
 
 def test_offense_record_arrainged(report, state):
-    string = "    ARRAIGNED SPEEDING(80 mph in a 65 mph zone)        INFRACTION    G.S. 20-141(B)"  # noqa
-    matches = offense.OffenseRecordRow(report, state).match(string)
+    string = """
+        ARRAIGNED SPEEDING(80 mph in a 65 mph zone)        INFRACTION    G.S. 20-141(B)
+    """
+    matches = offense.OffenseRecordRow(report, state, multiline=True).match(string)
     assert matches is not None, "Regex match failed"
     assert matches["action"] == "ARRAIGNED"
     assert matches["desc"] == "SPEEDING(80 mph in a 65 mph zone)"
@@ -41,25 +40,32 @@ def test_offense_record_arrainged(report, state):
 
 
 def test_offense_record_with_slashes(report, state):
-    string = "CONVICTED  CITY/TOWN VIOLATION (I)  INFRACTION  LOCAL ORDINANCE"
-    matches = offense.OffenseRecordRow(report, state).match(string)
+    string = """
+        CONVICTED  CITY/TOWN VIOLATION (I)  INFRACTION  LOCAL ORDINANCE
+    """
+    matches = offense.OffenseRecordRow(report, state, multiline=True).match(string)
     assert matches is not None, "Regex match failed"
     assert matches["desc"] == "CITY/TOWN VIOLATION (I)"
 
 
-def test_offense_record_with_multiline(report, state):
-    # pylint: disable=line-too-long
-    string = "\n    01 CHARGED   OPEN  CONT AFTER CONS    ALC 1ST(Blood Alcohol =    TRAFFIC    G.S. 20-138.7(A)\n NOT REQUIRED)  \n TEST         \n    CONVICTED"
+def test_offense_record_with_multiline_description(report, state):
+    string = """
+            01 CHARGED   OPEN  CONT AFTER CONS    ALC 1ST(Blood Alcohol =    TRAFFIC    G.S. 20-138.7(A)
+    NOT REQUIRED)
+    TEST
+            CONVICTED - - -
+    """
     matches = offense.OffenseRecordRowWithNumber(report, state, multiline=True).match(string)
-    test = re.sub(r'\s+', ' ', matches["desc_ext"].strip())
     assert matches is not None, "Regex match failed"
     assert matches["desc"] == "OPEN  CONT AFTER CONS    ALC 1ST(Blood Alcohol ="
-    assert test == "NOT REQUIRED) TEST"
+    assert matches["desc_ext"] == "NOT REQUIRED)\n    TEST"
 
 
 def test_offense_record_convicted(report, state):
-    string = "CONVICTED IMPROPER EQUIP - SPEEDOMETER             INFRACTION    G.S. 20-123.2"  # noqa
-    matches = offense.OffenseRecordRow(report, state).match(string)
+    string = """
+        CONVICTED IMPROPER EQUIP - SPEEDOMETER             INFRACTION    G.S. 20-123.2
+    """
+    matches = offense.OffenseRecordRow(report, state, multiline=True).match(string)
     assert matches is not None, "Regex match failed"
     assert matches["action"] == "CONVICTED"
     assert matches["desc"] == "IMPROPER EQUIP - SPEEDOMETER"
@@ -73,32 +79,38 @@ def test_offense_record_convicted(report, state):
         ("2000-01-01", "      Disposed on: 01/01/2000   "),
         (
             "2016-07-20",
-            "    Plea: RESPONSIBLE                         Verdict: RESPONSIBLE             Disposed on: 07/20/2016   ",
+            "\n    Plea: RESPONSIBLE                  Verdict: RESPONSIBLE             Disposed    on:   07/20/2016   ",
         ),
     ),
 )
 def test_offense_disposed_date(expected, val, report, state):
-    matches = offense.OffenseDisposedDate(report, state).match(val)
+    matches = offense.OffenseDisposedDate(report, state, multiline=True).match(val)
     assert matches is not None, "Regex match failed"
     assert matches["value"] == expected
 
 
 def test_known_offense_disposition_method(report, state):
-    string = "    Disposition Method: WAIVER - MAGISTRATE"
-    matches = offense.OffenseDispositionMethod(report, state).match(string)
+    string = """
+        Disposition Method: WAIVER - MAGISTRATE    
+    """
+    matches = offense.OffenseDispositionMethod(report, state, multiline=True).match(string)
     assert matches is not None, "Regex match failed"
     assert matches["value"] == "WAIVER - MAGISTRATE"
 
 
 def test_plea(report, state):
-    string = "Plea: NOT GUILTY                     Verdict:                             Disposed on: "
-    matches = offense.OffensePlea(report, state).match(string)
+    string = """
+        Plea: NOT GUILTY                     Verdict:                             Disposed on:    
+    """
+    matches = offense.OffensePlea(report, state, multiline=True).match(string)
     assert matches is not None, "Regex match failed"
     assert matches["value"] == "NOT GUILTY"
 
 
 def test_verdict(report, state):
-    string = "Plea: GUILTY                       Verdict: NOT GUILTY                       Disposed on: "
-    matches = offense.OffenseVerdict(report, state).match(string)
+    string = """
+        Plea: GUILTY                       Verdict: NOT GUILTY                       Disposed on:   
+    """
+    matches = offense.OffenseVerdict(report, state, multiline=True).match(string)
     assert matches is not None, "Regex match failed"
     assert matches["value"] == "NOT GUILTY"
