acbf72b68d7f405acec370f7125ceee10cb1134d
Rob Gries <robert.w.gries@gmail.com>
2021-07-14 00:58:08 -0400

    install_requires should work now for lark

--- setup.py
+++ setup.py
@@ -2,4 +2,4 @@ from setuptools import setup, find_packages
 
 import ciprs_reader
 
-setup(name="ciprs_reader", install_requires=['lark-parser'], version=ciprs_reader.VERSION, packages=find_packages())
+setup(name="ciprs_reader", install_requires=['lark-parser>=0.11.3'], version=ciprs_reader.VERSION, packages=find_packages())
