2ae87ed3851739480dccb3e4d51d396abf25bb2e
Rob Gries <robert.w.gries@gmail.com>
2021-07-25 12:52:36 -0400

    Run pre-commit autoupdate and pre-commit

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,40 +4,34 @@ import pytest
 from ciprs_reader.const import ParserMode
 from ciprs_reader.reader import PDFToTextReader
 
-EXPECTED_OUTPUT = [{
-    "General": {
-        "County": "DURHAM",
-        "File No": "88CR0121",
-        "District": "Yes"
-    },
-    "Case Information": {
-        "Case Status": "DISPOSED",
-        "Offense Date": "1988-05-01T00:00:00",
-        "Arrest Date": "1988-05-01"
-    },
-    "Defendant": {
-        "Name": "YA",
-        "Race": "WHITE",
-        "Sex": "F",
-        "Date of Birth/Estimated Age": ""
-    },
-    "District Court Offense Information": [
-        {
-            "Records": [
-                {
-                    "Action": "CHARGED",
-                    "Description": "POSSESS MARIJUANA UP TO 1/2 OZ",
-                    "Severity": "MISDEMEANOR",
-                    "Law": "G.S. 90-95(D)(4)"
-                }
-            ],
-            "Disposed On": "1988-10-07",
-            "Disposition Method": "DISMISSAL WITHOUT LEAVE BY DA"
-        }
-    ],
-    "Superior Court Offense Information": [],
-    "_meta": {}
-}]
+EXPECTED_OUTPUT = [
+    {
+        "General": {"County": "DURHAM", "File No": "88CR0121", "District": "Yes"},
+        "Case Information": {
+            "Case Status": "DISPOSED",
+            "Offense Date": "1988-05-01T00:00:00",
+            "Arrest Date": "1988-05-01",
+        },
+        "Defendant": {"Name": "YA", "Race": "WHITE", "Sex": "F", "Date of Birth/Estimated Age": ""},
+        "District Court Offense Information": [
+            {
+                "Records": [
+                    {
+                        "Action": "CHARGED",
+                        "Description": "POSSESS MARIJUANA UP TO 1/2 OZ",
+                        "Severity": "MISDEMEANOR",
+                        "Law": "G.S. 90-95(D)(4)",
+                    }
+                ],
+                "Disposed On": "1988-10-07",
+                "Disposition Method": "DISMISSAL WITHOUT LEAVE BY DA",
+            }
+        ],
+        "Superior Court Offense Information": [],
+        "_meta": {},
+    }
+]
+
 
 @pytest.mark.parametrize(
     "parserMode",
