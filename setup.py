c75d4b1048c505d41501abb22774522aa00b9c4c
Colin Copeland <copelco@caktusgroup.com>
2021-10-09 22:26:07 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,4 +2,8 @@ from setuptools import setup, find_packages
 
 import ciprs_reader
 
+import site
+import sys
+site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
+
 setup(name="ciprs_reader", version=ciprs_reader.VERSION, packages=find_packages())
